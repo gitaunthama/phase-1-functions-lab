4b1de13845cf5c26aa16299d416ca428a23619e6
gitaunthama <gitaunthama@gmail.com>
2024-03-20 13:08:41 +0300

    completer work

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks( distance){
+    const headquarterslocation=42
+    
+    return Math.abs(headquarterslocation-distance);
+    
+}
+function distanceFromHqInFeet( distance){
+    const headquarterslocation=42;
+
+    const feetperblock=264;
+
+    const distanceinblocks=Math.abs(distance-headquarterslocation);
+
+    const distanceinfeet=distanceinblocks*feetperblock;
+    
+    return distanceinfeet;
+
+    
+}
+
+function distanceTravelledInFeet(start, end){
+
+    const feetperblock=264;
+
+    const distanceinblocks=Math.abs(end-start);
+
+    const distanceinfeet=distanceinblocks*feetperblock;
+    
+    return distanceinfeet;
+
+    
+}
+
+
+function calculatesFarePrice(start, destination){
+    const feetperblock=264;
+
+    const distanceinblocks=Math.abs(destination-start);
+
+    const distanceInFeet=distanceinblocks*feetperblock;
+
+     
+    let price = 0;
+
+  if (distanceInFeet <= 400) {
+    price= 0; 
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    price = (distanceInFeet - 400) *2/100; 
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    price = 25; 
+  } else {
+    price = 'cannot travel that far';
+  }
+
+  return price;
+}
+
+
+    
